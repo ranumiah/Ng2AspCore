a78711561cccdf6c03a6106403f201161b16c8b8
Ranu <O@O.net>
2018-02-02 14:55:20 +0000

    Add EF 2.0 Core package and DBModel

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Ng2AspCore.DBModel;
 
 namespace Ng2AspCore
 {
@@ -23,6 +25,7 @@ namespace Ng2AspCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HRContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MsSqlDbConnection")));
             services.AddMvc();
         }
 
